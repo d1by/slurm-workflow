67c76d233e6856a6d4f43a52e37a7458c0b61fda
tintedfireglass <tintedfireglass@hotmail.com>
2023-05-24 19:19:14 +0530

    Added untested slurm-mongo script

---
+++ slurm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=job_%j
+#SBATCH --output=job_%j.out
+#SBATCH --error=job_%j.err
+#SBATCH --partition=test
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:10:00
+
+numactl --interleave=all mongod --dbpath=database --bind_ip $(hostname -i) &
+python3 mongo.py
+#note:python script should have this line to work var = pymongo.MongoClient(host=<IP of MongoDB Server>)
